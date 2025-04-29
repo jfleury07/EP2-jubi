64c4e0948a282923e3d13b3bedc4199753d2277c
jfleury07 <julia.nigrin@gmail.com>
2025-04-29 17:42:27 -0300

    rolar_dados

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,9 @@
-print ('oi')
-print ('Jubi')
+
+import random
+def rolar_dados (dados): 
+    dados_rolados = []
+    for rolagem in range (1,dados+1): 
+        dado_rolado = random.randint (1,6)
+        dados_rolados.append (dado_rolado)
+    return dados_rolados
+
